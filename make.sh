90d12cbf0757d8601ce57b3e09cc9f5d06b5f396
David Matlack <matlackdavid@gmail.com>
2013-05-25 00:18:18 -0400

    Better urxvt border color

--- make.sh
+++ make.sh
@@ -7,7 +7,7 @@ files="bashrc vimrc vim tmux.conf"
 
 # Files specific to crunchbang install
 if [ `hostname` = crunchbang ]; then
-  files="$files conkyrc"
+  files="$files conkyrc Xdefaults"
 fi
 
 ##########
