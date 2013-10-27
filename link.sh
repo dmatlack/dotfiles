2207a91deff118a3a55fcb04426a406945cc8d79
David Matlack <matlackdavid@gmail.com>
2013-10-27 10:36:15 -0700

    added i3, renamed make.sh to link.sh

---
+++ link.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+dir="$HOME/.dotfiles"
+
+# These files should be added for all my computers
+files="bashrc vimrc vim tmux.conf irssi gitconfig 
+       Xdefaults Xmodmap xbindkeysrc i3"
+
+##########
+
+(
+  echo -n "Changing to the $dir directory ... "
+  cd $dir
+  echo "done"
+  
+  for file in $files; do
+    rm ~/.$file
+    echo "Creating symlink to $file in home directory."
+    ln -s $dir/$file ~/.$file
+  done
+)
